b3e8d7ff6a6dd173f3464326df766f352089fad6
PaulKMandal <p.mandal@aol.com>
2023-09-26 12:00:49 -0500

    Added dataloaders for Segmentation.

---
+++ fate/python/federatedml/nn/dataset/segmentation/vertutils.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+from matplotlib import pyplot as plt
+import pandas as pd
+import numpy as np
+import scipy.misc
+import random
+import os
+import csv
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import utils
+from federatedml.nn.dataset.segmentation import image_transforms
+from PIL import Image
+from torchvision.transforms import functional as F
+import numpy.lib.recfunctions as nlr
+
+root_dir = "CamVid/"
+data_dir = os.path.join(root_dir, "train")
+label_dir = os.path.join(root_dir, "train_labels")
+label_colors_file = os.path.join(root_dir, "class_dict.csv")
+val_data_dir = os.path.join(root_dir, "val")
+val_label_dir = os.path.join(root_dir, "val_labels")
+
+num_class = 32
+means = np.array([103.939, 116.779, 123.68]) / 255.  # mean of three channels in the order of BGR
+h,w = 128,128
+
+
+val_h = int(h / 32) * 32  # 704
+val_w = w  # 960
+class_to_regress = 18 #road
+
+
+class SegmentationLabel(Dataset):
+    def __init__(self, seg_dir, map_file, transform=image_transforms.LblToTensor()):
+        from glob import glob
+        from os import path
+        self.seg_dir = seg_dir
+        self.files = []
+        self.crop = True
+        self.color_to_class = {}
+        self.class_to_color = {}
+        self.n_class = 32
+        #self.h = 512
+        #self.w = 512
+        self.h = 128
+        self.w = 128
+        
+        with open(map_file, 'r') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row_num, row in enumerate(reader, start=1):
+                color = (int(row['r']), int(row['g']), int(row['b']))
+                # class_name = row['name']
+                self.color_to_class[color] = row_num - 1  # class_name
+
+            self.n_class = len(self.color_to_class)
+
+        self.class_to_color = {v: k for k, v in self.color_to_class.items()}
+        
+        for im_f in glob(path.join(seg_dir, '*.png')):
+            self.files.append(os.path.basename(im_f))
+            # self.files.append(im_f.replace('_im.jpg', ''))
+
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, idx):
+        b = self.files[idx]
+
+        lbl = Image.open(self.seg_dir + b)
+
+        lbl = lbl.resize((self.h,self.w))
+
+
+        
+        if self.transform is not None:
+            lbl = self.transform(lbl)
+
+        # print("lbl0",lbl.size())
+
+        lbl = lbl.permute(1, 2, 0)
+        lbl = lbl.numpy()
+        lbl = nlr.unstructured_to_structured(lbl).astype('O')
+        dummy_class = 31 #Change dummy class to whatever class you want colors not defined in your dictinoary to be
+        lbl = torch.tensor(np.vectorize(self.color_to_class.get)(lbl, dummy_class), dtype=torch.uint8)
+
+        # create one-hot encoding
+
+        target = torch.nn.functional.one_hot(lbl.to(torch.int64), num_classes=self.n_class)
+        target = target.permute(2, 0, 1)
+        target = target.float()
+        
+        return target[17]
+
+class SegmentationImage(Dataset):
+    def __init__(self, img_dir, transform=image_transforms.ImgToTensor()):
+        from glob import glob
+        from os import path
+        self.img_dir = img_dir
+        self.files = []
+        self.crop = True
+        self.color_to_class = {}
+        self.class_to_color = {}
+        self.n_class = 32
+        self.h = 128
+        self.w = 128
+        
+        for im_f in glob(path.join(img_dir, '*.png')):
+            self.files.append(os.path.basename(im_f))
+            # self.files.append(im_f.replace('_im.jpg', ''))
+
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, idx):
+        b = self.files[idx]
+        img = Image.open(self.img_dir + b)
+
+        img = img.resize((self.h,self.w))
+
+        if self.transform is not None:
+            img = self.transform(img)
+        
+        return img
+
+def show_batch(batch):
+    img_batch = batch['X']
+    img_batch[:, 0, ...].add_(means[0])
+    img_batch[:, 1, ...].add_(means[1])
+    img_batch[:, 2, ...].add_(means[2])
+    batch_size = len(img_batch)
+
+    grid = utils.make_grid(img_batch)
+    plt.imshow(grid.numpy()[::-1].transpose((1, 2, 0)))
+
+    plt.title('Batch from dataloader')
+
+
+def get_transform():
+    custom_transforms = []
+    custom_transforms.append(torchvision.transforms.ToTensor())
+    return torchvision.transforms.Compose(custom_transforms)
+
+
+def collate_fn(batch):
+    return tuple(zip(*batch))
---
+++ fate/python/federatedml/nn/dataset/vertimg.py
@@ -0,0 +1,83 @@
+import torch
+from federatedml.nn.dataset.base import Dataset
+from torchvision.datasets import ImageFolder
+from torchvision import transforms
+import numpy as np
+
+#from federatedml.nn.dataset.segmentation.horizutils import CamVidDataset, get_transform
+
+from federatedml.nn.dataset.segmentation.vertutils import SegmentationImage, get_transform
+from federatedml.nn.dataset.segmentation import image_transforms
+
+from federatedml.util import LOGGER
+
+
+class VertSegImg(Dataset):
+    
+    """
+
+    A basic Image Dataset built on pytorch ImageFolder, supports simple image transform
+    Given a folder path, ImageDataset will load images from this folder, images in this
+    folder need to be organized in a Torch-ImageFolder format, see
+    https://pytorch.org/vision/main/generated/torchvision.datasets.ImageFolder.html for details.
+
+    Image name will be automatically taken as the sample id.
+
+    Parameters
+    ----------
+    center_crop : bool, use center crop transformer
+    center_crop_shape: tuple or list
+    generate_id_from_file_name: bool, whether to take image name as sample id
+    file_suffix: str, default is '.jpg', if generate_id_from_file_name is True, will remove this suffix from file name,
+                 result will be the sample id
+    return_label: bool, return label or not, this option is for host dataset, when running hetero-NN
+    float64: bool, returned image tensors will be transformed to double precision
+    label_dtype: str, long, float, or double, the dtype of return label
+    """
+
+    def __init__(self, center_crop=False,
+                 generate_id_from_file_name=True, file_suffix='.png',
+                 return_label=True, float64=False, label_dtype='long'):
+
+        super(VertSegImg, self).__init__()
+        self.segmentations: SegmentationImage = None
+        self.return_label = return_label
+        self.generate_id_from_file_name = generate_id_from_file_name
+        self.file_suffix = file_suffix
+        self.float64 = float64
+        self.dtype = torch.float32 if not self.float64 else torch.float64
+        avail_label_type = ['float', 'long', 'double']
+        self.sample_ids = None
+        assert label_dtype in avail_label_type, 'available label dtype : {}'.format(
+            avail_label_type)
+        if label_dtype == 'double':
+            self.label_dtype = torch.float64
+        elif label_dtype == 'long':
+            self.label_dtype = torch.int64
+        else:
+            self.label_dtype = torch.float32
+
+    def load(self, path):
+
+        # read image from folders
+        self.segmentations = SegmentationImage(img_dir=path)
+
+    def __getitem__(self, item):
+        #need to do this because FATE requires a bottom model even if there is no data on the federate
+        return torch.zeros(1).type(self.dtype), self.segmentations.__getitem__(item)
+
+    def __len__(self):
+        return self.segmentations.__len__()
+
+    def __repr__(self):
+        return self.segmentations.__repr__()
+
+    def get_classes(self):
+        return self.segmentations.__len__()
+
+    def get_sample_ids(self):
+        return self.sample_ids
+
+
+if __name__ == '__main__':
+    pass
